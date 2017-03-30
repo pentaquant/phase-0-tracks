c5d5555568114be9ed42235396ca3f949a6eae9d
pentaquant <pjsmueller@gmail.com>
2017-03-29 20:27:19 -0700

    added code

---
+++ ruby/hamster.rb
@@ -0,0 +1,28 @@
+puts "What is the hamster's name?"
+name = gets.chomp
+puts "The hamster's name is #{name}"
+
+puts "How loud is the hamster, from 1 to 10?"
+volume = gets.chomp.to_i
+puts "The creature's volume is #{volume}"
+
+puts "What is its fur color?"
+furcolor = gets.chomp
+puts "The hamster has #{furcolor} fur"
+
+puts "Is this hamster a good or bad candidate?"
+candidate = gets.chomp
+if candidate == "good"
+  puts "We'd be happy to have you for adoption!"
+elsif candidate == "bad" 
+  puts "We're sorry, maybe you should look into other shelters"
+end
+
+
+puts "How old is the hamster?"
+age = gets.chomp.to_i
+if age == 0
+  puts nil
+else
+  puts "The hamster is #{age} years old"
+end 
