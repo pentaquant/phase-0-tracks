354d54d7f8fc8183049cbe1a48b1098f0ccea02e
pentaquant <pjsmueller@gmail.com>
2017-04-08 14:35:19 -0700

    began alias_manager.rb

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,18 @@
+#take a spys real full name
+puts "What is your name?"
+  og_name = gets.chomp.to_s
+
+#Swap the first and last name
+og_name.split(' ')
+
+swamp_name.split('')
+
+x = swamp_name.length
+
+p swap_name
+
+p x
+
+
+
+#Change vowel to next vowel (aeiou) and any consonant to the next letter in the alphabet
