b10c460de41014a2492e8eb5b0fe0190fd85312d
pentaquant <pjsmueller@gmail.com>
2017-04-11 12:16:24 -0700

    Finalized 6.1

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,18 +1,16 @@
-puts "This is 6.1"
+SPECIES ------------------------
+Puppy
 
-# SPECIES ------------------------
-# Puppy
+CHARACTERISTICS ----------------
+Bark decibels: varies
+Friendly: yes
+Intelligent: sometimes
+Body Size: small
+Name: varies
+Color: varies
 
-# CHARACTERISTICS ----------------
-# Bark decibels: varies
-# Friendly: yes
-# Intelligent: sometimes
-# Body Size: small
-# Name: varies
-# Color: varies
-
-# BEHAVIOR -----------------------
-# Bark
-# Cuddle
-# Drool
-# Nap
+BEHAVIOR -----------------------
+Bark
+Cuddle
+Drool
+Nap
