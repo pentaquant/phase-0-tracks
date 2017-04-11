e2fc71d19e8789d284d489cd85aec566c6d5558f
pentaquant <pjsmueller@gmail.com>
2017-04-11 12:15:37 -0700

    completed 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+puts "This is 6.1"
+
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Bark decibels: varies
+# Friendly: yes
+# Intelligent: sometimes
+# Body Size: small
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Bark
+# Cuddle
+# Drool
+# Nap
