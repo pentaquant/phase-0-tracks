279d75cd4a5779643d7d4ff480dc9b526d9544e3
pentaquant <pjsmueller@gmail.com>
2017-04-14 11:19:31 -0700

    Finalized 6.4

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,25 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + ":("
+#   end
+# end
+
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def yelling(words_to_yell)
+    puts "#{words_to_yell}"
+  end
 end
+
+class Teacher
+  include Shout
+end
+
+class Auctioneer
+  include Shout
+end
+
+teacher = Teacher.new
+teacher.yelling("Put the paste down now")
+
+auctioneer = Auctioneer.new
+auctioneer.yelling("Sold to the lady in the red dress")
