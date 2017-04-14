892c9e38d00ae0308ad7e0185a6f30386ae46518
pentaquant <pjsmueller@gmail.com>
2017-04-13 17:29:38 -0700

    Finalized 6.3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,26 +1,25 @@
 class Santa
-#getter and setter shortcuts
+#added getter and setter shortcuts
   attr_reader :age, :ethnicity
   attr_accessor :gender, :age
-
-#initialized new classes
+#Initializing new instances with a rand function on age
   def initialize(gender, ethnicity)
     puts "Initializing Santa instance....."
     @gender = gender
     @ethnicity = ethnicity
-    @age = 0
+    @age = rand(0..140)
     reindeer_rankings = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
-  
+  #Get_mad method sending the reindeer name fed through to the last of the array
   def get_mad_at(reindeer_name)
     reindeer_rankings.delete(reindeer_name)
     reindeer_rankings << reindeer_name
   end
   
-  def gender=(new_gender)
-    @gender = new_gender
-    puts "This santa is now #{@gender} gender"
-  end
+#Code below removed because of attr_accessor  
+  # def gender=(new_gender)
+  #   @gender = new_gender
+  # end
   
   def celebrate_birthday
     @age += 1
@@ -36,240 +35,13 @@ class Santa
   end
 end
 
-genders = ["Bigendered",
-"Crossdresser",
-"Drag King",
-"Drag Queen",
-"Femme Queen",
-"Female-to-Male",
-"FTM",
-"Gender Bender",
-"Genderqueer",
-"Male-to-Female",
-"MTF",
-"Non-Op",
-"HIJRA",
-"Pangender",
-"Transexual/Transsexual",
-"Trans Person",
-"Woman",
-"Man",
-"Butch",
-"Two-Spirit",
-"Trans",
-"Agender",
-"Third Sex",
-"Gender Fluid",
-"Non-Binary Transgender",
-"Androgyne",
-"Gender Gifted",
-"Gender Blender",
-"Femme",
-"Person of Transgender Experience",
-"Androgynous"]
+genders = ["Bigendered","Crossdresser","Drag King","Drag Queen","Femme Queen","Female-to-Male","FTM","Gender Bender","Genderqueer","Male-to-Female","MTF","Non-Op","HIJRA","Pangender","Transexual/Transsexual","Trans Person","Woman","Man","Butch","Two-Spirit","Trans","Agender","Third Sex","Gender Fluid","Non-Binary Transgender","Androgyne","Gender Gifted","Gender Blender","Femme","Person of Transgender Experience","Androgynous"]
 
-
-ethnicities = [
-  "Afghans",
-  "Albanians",
-  "Algerians",
-  "Americans",
-  "Andorrans",
-  "Angolans",
-  "Argentines",
-  "Armenians",
-  "Aromanians",
-  "Arubans",
-  "Australians",
-  "Austrians",
-  "Azerbaijanis",
-  "Bahamians",
-  "Bahrainis",
-  "Bangladeshis",
-  "Barbadians",
-  "Basotho",
-  "Basques",
-  "Belarusians",
-  "Belgians",
-  "Belizeans",
-  "Bermudians",
-  "Boers",
-  "Bosniaks",
-  "Brazilians",
-  "Bretons",
-  "British",
-  "British Virgin Islanders",
-  "Bruneians",
-  "Bulgarians",
-  "Macedonian Bulgarians",
-  "Burkinabès",
-  "Burundians",
-  "Cambodians",
-  "Cameroonians",
-  "Canadians",
-  "Catalans",
-  "Cape Verdeans",
-  "Chadians",
-  "Chileans",
-  "Chinese",
-  "Colombians",
-  "Comorians",
-  "Congolese",
-  "Croatians",
-  "Cubans",
-  "Cypriots",
-  "Turkish Cypriots",
-  "Czechs",
-  "Danes",
-  "Dominicans (Republic)",
-  "Dominicans (Commonwealth)",
-  "Dutch",
-  "East Timorese",
-  "Ecuadorians",
-  "Egyptians",
-  "Emiratis",
-  "English",
-  "Eritreans",
-  "Estonians",
-  "Ethiopians",
-  "Falkland Islanders",
-  "Faroese",
-  "Finns",
-  "Finnish Swedish",
-  "Fijians",
-  "Filipinos",
-  "French citizens",
-  "Georgians",
-  "Germans",
-  "Baltic Germans",
-  "Ghanaians",
-  "Gibraltar",
-  "Greeks",
-  "Greek Macedonians",
-  "Grenadians",
-  "Guatemalans",
-  "Guianese (French)",
-  "Guineans",
-  "Guinea-Bissau nationals",
-  "Guyanese",
-  "Haitians",
-  "Hondurans",
-  "Hong Kong",
-  "Hungarians",
-  "Icelanders",
-  "I-Kiribati",
-  "Indians",
-  "Indonesians",
-  "Iranians",
-  "Iraqis",
-  "Irish",
-  "Israelis",
-  "Italians",
-  "Ivoirians",
-  "Jamaicans",
-  "Japanese",
-  "Jordanians",
-  "Kazakhs",
-  "Kenyans",
-  "Koreans",
-  "Kosovars",
-  "Kurds",
-  "Kuwaitis",
-  "Kyrgyzs",
-  "Lao",
-  "Latvians",
-  "Lebanese",
-  "Liberians",
-  "Libyans",
-  "Liechtensteiners",
-  "Lithuanians",
-  "Luxembourgers",
-  "Macao",
-  "Macedonians",
-  "Malagasy",
-  "Malaysians",
-  "Malawians",
-  "Maldivians",
-  "Malians",
-  "Maltese",
-  "Manx",
-  "Mauritians",
-  "Mexicans",
-  "Moldovans",
-  "Moroccans",
-  "Mongolians",
-  "Montenegrins",
-  "Namibians",
-  "Nepalese",
-  "New Zealanders",
-  "Nicaraguans",
-  "Nigeriens",
-  "Nigerians",
-  "Norwegians",
-  "Pakistanis",
-  "Palauans",
-  "Palestinians",
-  "Panamanians",
-  "Papua New Guineans",
-  "Paraguayans",
-  "Peruvians",
-  "Poles",
-  "Portuguese",
-  "Puerto Ricans",
-  "Quebecers",
-  "Réunionnais",
-  "Romanians",
-  "Russians",
-  "Baltic Russians",
-  "Rwandans",
-  "Salvadorans",
-  "São Tomé and Príncipe",
-  "Saudis",
-  "Scots",
-  "Senegalese",
-  "Serbs",
-  "Sierra Leoneans",
-  "Singaporeans",
-  "Sindhian",
-  "Slovaks",
-  "Slovenes",
-  "Somalis",
-  "Somalilanders",
-  "South Africans",
-  "Spaniards",
-  "Sri Lankans",
-  "St Lucians",
-  "Sudanese",
-  "Surinamese",
-  "Swedes",
-  "Swiss",
-  "Syriacs",
-  "Syrians",
-  "Tajik",
-  "Taiwanese",
-  "Tanzanians",
-  "Thais",
-  "Tibetans",
-  "Tobagonians",
-  "Trinidadians",
-  "Tunisians",
-  "Turks",
-  "Tuvaluans",
-  "Ugandans",
-  "Ukrainians",
-  "Uruguayans",
-  "Uzbeks",
-  "Vanuatuans",
-  "Venezuelans",
-  "Vietnamese",
-  "Welsh",
-  "Yemenis",
-  "Zambians",
-  "Zimbabweans"
-]
+ethnicities = ["Afghans","Albanians","Algerians","Americans","Andorrans","Angolans","Argentines","Armenians","Aromanians","Arubans","Australians","Austrians","Azerbaijanis","Bahamians","Bahrainis","Bangladeshis","Barbadians","Basotho","Basques","Belarusians","Belgians","Belizeans","Bermudians","Boers","Bosniaks","Brazilians","Bretons","British","British Virgin Islanders","Bruneians","Bulgarians","Macedonian Bulgarians","Burkinabès","Burundians","Cambodians","Cameroonians","Canadians","Catalans","Cape Verdeans","Chadians","Chileans","Chinese","Colombians","Comorians","Congolese","Croatians","Cubans","Cypriots","Turkish Cypriots","Czechs","Danes","Dominicans (Republic)","Dominicans (Commonwealth)","Dutch","East Timorese","Ecuadorians","Egyptians","Emiratis","English","Eritreans","Estonians","Ethiopians","Falkland Islanders","Faroese","Finns","Finnish Swedish","Fijians","Filipinos","French citizens","Georgians","Germans","Baltic Germans","Ghanaians","Gibraltar","Greeks","Greek Macedonians","Grenadians","Guatemalans","Guianese (French)","Guineans","Guinea-Bissau nationals","Guyanese","Haitians","Hondurans","Hong Kong","Hungarians","Icelanders","I-Kiribati","Indians","Indonesians","Iranians","Iraqis","Irish","Israelis","Italians","Ivoirians","Jamaicans","Japanese","Jordanians","Kazakhs","Kenyans","Koreans","Kosovars","Kurds","Kuwaitis","Kyrgyzs","Lao","Latvians","Lebanese","Liberians","Libyans","Liechtensteiners","Lithuanians","Luxembourgers","Macao","Macedonians","Malagasy","Malaysians","Malawians","Maldivians","Malians","Maltese","Manx","Mauritians","Mexicans","Moldovans","Moroccans","Mongolians","Montenegrins","Namibians","Nepalese","New Zealanders","Nicaraguans","Nigerians","Norwegians","Pakistanis","Palauans","Palestinians","Panamanians","Papua New Guineans","Paraguayans","Peruvians","Poles","Portuguese","Puerto Ricans","Quebecers","Réunionnais","Romanians","Russians","Baltic Russians","Rwandans","Salvadorans","São Tomé and Príncipe","Saudis","Scots","Senegalese","Serbs","Sierra Leoneans","Singaporeans","Sindhian","Slovaks","Slovenes","Somalis","Somalilanders","South Africans","Spaniards","Sri Lankans","St Lucians","Sudanese","Surinamese","Swedes","Swiss","Syriacs","Syrians","Tajik","Taiwanese","Tanzanians","Thais","Tibetans","Tobagonians","Trinidadians","Tunisians","Turks","Tuvaluans","Ugandans","Ukrainians","Uruguayans","Uzbeks","Vanuatuans","Venezuelans","Vietnamese","Welsh","Yemenis","Zambians","Zimbabweans"]
 
 puts "How many santas would you like to create"
 santa_amount = gets.chomp.to_i
+#Added counter to differentiate between the instances
 santa_id = 1
  santa_amount.times do 
    santa = Santa.new(genders.sample, ethnicities.sample)
@@ -279,3 +51,4 @@ santa_id = 1
    puts "Santa ID: #{santa_id}"
   santa_id += 1
 end
+
