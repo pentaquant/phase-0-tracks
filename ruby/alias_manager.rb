84a64d1e6a39c54963b1f73045e97f2bde128e11
pentaquant <pjsmueller@gmail.com>
2017-04-19 18:34:11 -0700

    alias_manager.rb; char conversion is working, need to work out edge cases and join methods

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -15,9 +15,24 @@ def alias_manager(name_in)
   vowels = "aeiou"
   consonants = "bcdfghjklmnpqrstvwxyz"
 
-  r_name = name_in.downcase.split(' ').reverse.join(' ')
+  r_name = name_in.downcase.split(' ').reverse.join(' ').chars
 
+  n = r_name.length
+  p r_name
+  #check this might be problematic, maybe ...
+  i = 0
+  while i < n
+      if vowels.include? r_name[i]
+        r_name[i] = vowels[vowels.index(r_name[i])+1]
+        i += 1
+      elsif consonants.include? r_name[i]
+        r_name[i] = consonants[consonants.index(r_name[i])+1]
+        i += 1
+      else 
+        i += 1
+      end
+  end
+  p r_name
 end
   
 alias_manager("Peter Mueller")
-#DRIVER CODE________________
